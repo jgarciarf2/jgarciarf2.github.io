6acf5e42e6db841bdc29aac2d4ef7967e0bf8b57
Juan Fernando Garcia <jgarciarf216@gmail.com>
2025-03-10 18:57:03 -0500

    Create component Home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, HomeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'jgarciarf2.github.io';
+  title = 'personal-portfolio';
 }
